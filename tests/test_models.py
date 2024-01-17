2dc301e9c6455233602b6502a00e981564a3a540
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2024-01-17 11:44:30 +0800

    Pull from transifex

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,20 @@
 import pytest
 from mock import Mock, patch, call
 
-from .fixtures import engine_mock, config_mock, guards_e
+from .fixtures import engine_mock, config_mock, guards_engine_mock
+
+
+@pytest.mark.usefixtures('engine_mock')
+class TestHelperFunctions(object):
+
+    @patch('ramses.models.engine')
+    def test_get_existing_model_not_found(self, mock_eng):
+        from ramses import models
+        mock_eng.get_document_cls.side_effect = ValueError
+        model_cls = models.get_existing_model('Foo')
+        assert model_cls is None
+        mock_eng.get_document_cls.assert_called_once_with('Foo')
+
+    @patch('ramses.models.engine')
+    def test_get_existing_model_found(self, mock_eng):
+        from 
