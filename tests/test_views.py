2e7146ddd06fcd6c5159a35ba8b277c90733b96c
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2024-06-15 19:14:24 +0800

    Ignore logs

--- tests/test_views.py
+++ tests/test_views.py
@@ -9,4 +9,26 @@ from ramses import views
 from .fixtures import config_mock, guards_engine_mock
 
 
-class ViewTestBase(obj
+class ViewTestBase(object):
+    view_cls = None
+    view_kwargs = dict(
+        context={},
+        _query_params={'foo': 'bar'},
+        _json_params={'foo2': 'bar2'},
+    )
+    request_kwargs = dict(
+        method='GET',
+        accept=[''],
+    )
+
+    def _test_view(self):
+        class View(self.view_cls, BaseView):
+            _json_encoder = 'foo'
+
+        request = Mock(**self.request_kwargs)
+        return View(request=request, **self.view_kwargs)
+
+
+class TestSetObjectACLMixin(object):
+    def test_set_object_acl(self, guards_engine_mock):
+        view = v
