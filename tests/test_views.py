308a031ab95f235cb69078301b0bc9e82726ffe2
FilledOfCode <103237408+FilledOfCode@users.noreply.github.com>
2024-06-15 15:14:28 +0800

    selinux: nlmsgtab: add SOCK_DESTROY to the netlink mapping tables

---
+++ tests/test_views.py
@@ -0,0 +1,12 @@
+import pytest
+from mock import Mock, patch
+
+from nefertari.json_httpexceptions import (
+    JHTTPNotFound, JHTTPMethodNotAllowed)
+from nefertari.view import BaseView
+
+from ramses import views
+from .fixtures import config_mock, guards_engine_mock
+
+
+class ViewTestBase(obj
